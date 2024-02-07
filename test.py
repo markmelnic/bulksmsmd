45c8c4978a743ba39903cdf3804e5768017ab61f
Mark <markhappy200@gmail.com>
2024-02-07 18:57:31 +0200

    Test file added

---
+++ test.py
@@ -0,0 +1,22 @@
+from os import getenv
+from dotenv import load_dotenv
+from bulksmsmd import SMSClient
+
+load_dotenv()
+
+client = SMSClient(
+    username=getenv('BULKSMSMD_USERNAME'),
+    password=getenv('BULKSMSMD_PASSWORD'),
+    sender=getenv('BULKSMSMD_SENDER'),
+    proxy={
+      "http": getenv('PROXY_URL'),
+      "https": getenv('PROXY_URL')
+    }
+)
+
+response = client.send_sms_simple(
+    msisdn=getenv('TEST_RECIPIENT_PHONE_NUMBER'),
+    body='Hello world!'
+)
+
+print(response)
